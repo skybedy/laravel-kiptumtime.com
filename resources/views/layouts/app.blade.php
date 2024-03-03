7391734136a9bb9e1414d79b8e39b6dc287f3f9a
skybedy <skybedy@gmail.com>
2024-03-03 23:42:50 +0100

    030324

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-     
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -52,32 +52,35 @@ input[type=file]::file-selector-button {
 input[type=file]::file-selector-button:hover {
   background: #0d45a5;
 }
-       
-       
-       
+
+
+
        .architects-daughter-regular {
   font-family: "Architects Daughter", cursive;
   font-weight: 400;
   font-style: normal;
 }
-       
-       
+
+
         </style>
 
 
 
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen">
+        <div class="flex flex-col h-screen justify-between">
 
             @include('layouts.navigation')
 
 
             <!-- Page Content -->
-            <main>
+            <main class="mb-auto">
 
                 {{ $slot }}
             </main>
+            <footer class="h-20 border-t-2 border-gray-800 text-center flex items-center justify-center bg-[#fefdf9] ">
+                <p class="text-lg xs:text-2xl sm:text-3xl text-gray-800 font-black">Dedicated to the memory of Kelvin Kiptum</p>
+            </footer>
         </div>
 
     </body>
