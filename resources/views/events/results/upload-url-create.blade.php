1968e80689a5989030e719d50f9f8c99f2a2a925
skybedy <skybedy@gmail.com>
2024-04-03 21:56:46 +0000

    Vzhled

--- resources/views/events/results/upload-url-create.blade.php
+++ resources/views/events/results/upload-url-create.blade.php
@@ -32,7 +32,7 @@
             <form class="border border-gray-900 rounded-md  p-3 sm:p-4 md:p-6 mt-10 bg-green-700" action="{{ route('event.upload.store.url',$eventId) }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="flex space-x-2">
-                    <input type="text" name="strava_url" class="text-white text-[0.7rem] sm:text-xl xl:text-2xl w-2/3 sm:w-full border border-gray-900  rounded-md py-[20px]" placeholder="www.strava.com/activities/12345678">
+                    <input type="text" name="strava_url" class="text-gray-900 text-[0.7rem] sm:text-xl xl:text-2xl w-2/3 sm:w-full border border-gray-900  rounded-md py-[20px]">
                     <button type="submit" class="flex items-center justify-center bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-1 rounded-md w-1/3 sm:w-1/5 text-xs sm:text-sm md:text-lg">
                         Upload link
                     </button>
